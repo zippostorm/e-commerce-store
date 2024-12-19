834b65647f8dc20f4e06b2640a240ec263ef484c
zippostorm <toghkklli@gmail.com>
2024-12-19 15:18:32 +0200

    database setup complete

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
