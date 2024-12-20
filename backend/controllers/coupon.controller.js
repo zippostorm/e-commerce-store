4f2e5a84f58b37c775b6d00a52848719b451499f
zippostorm <toghkklli@gmail.com>
2024-12-20 17:29:09 +0200

    add coupon model/api route and logic

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,41 @@
+import Coupon from "../models/coupon.model.js";
+
+export const getCoupon = async (req, res) => {
+  try {
+    const coupon = await Coupon.findOne({
+      userId: req.user._id,
+      isActive: true,
+    });
+    res.status(200).json(coupon || null);
+  } catch (error) {
+    console.log("Error getting coupon:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const coupon = await Coupon.findOne({
+      code: code,
+      userId: req.user._id,
+      isActive: true,
+    });
+    if (!coupon) return res.status(404).json({ message: "Coupon not found" });
+
+    if (coupon.expirationDate < new Date()) {
+      coupon.isActive = false;
+      await coupon.save();
+      return res.status(400).json({ message: "Coupon expired" });
+    }
+
+    res.status(200).json({
+      message: "Coupon is valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log("Error validating coupon:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
