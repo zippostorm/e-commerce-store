3a529f9679d3894a08a0b2311d573771a40f43a4
zippostorm <toghkklli@gmail.com>
2025-01-15 15:43:28 +0200

    coupon fix

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -116,12 +116,16 @@ async function createStripeCoupon(discountPercentage) {
 }
 
 async function createNewCoupon(userId) {
+  await Coupon.findOneAndDelete({ userId });
+
   const newCoupon = new Coupon({
     code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
     discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
     userId: userId,
   });
 
   await newCoupon.save();
+
   return newCoupon;
 }
