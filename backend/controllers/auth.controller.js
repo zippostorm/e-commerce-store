77146a49243e47a43cef507c14818c9897f712ae
zippostorm <toghkklli@gmail.com>
2025-01-20 15:35:48 +0200

    fix User controller import at the auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import { redis } from "../lib/redis.js";
-import User from "../models/User.model.js";
+import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 const generateTokens = (userId) => {
